778f32418391e948a95b057778c75facbd649434
RainbowWolfer <rainbowwolfer@outlook.com>
2025-01-10 21:33:50 +0800

    Add project files.

---
+++ KeyBoardMouseHookWFDemo/MethodWindowsHook.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace KeyBoardMouseHookWFDemo;
+
+[SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "<Pending>")]
+[SuppressMessage("Interoperability", "SYSLIB1054:Use 'LibraryImportAttribute' instead of 'DllImportAttribute' to generate P/Invoke marshalling code at compile time", Justification = "<Pending>")]
+public class MethodWindowsHook : IMethod {
+
+	private bool isControlDown = false;
+
+	private readonly IntPtr hookID;
+
+	public MethodWindowsHook() {
+		hookID = SetHook(HookCallback);
+	}
+
+	public void Dispose() {
+		GC.SuppressFinalize(this);
+		UnhookWindowsHookEx(hookID);
+	}
+
+	private static IntPtr SetHook(LowLevelKeyboardProc proc) {
+		using Process curProcess = Process.GetCurrentProcess();
+		using ProcessModule? curModule = curProcess.MainModule;
+		return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule!.ModuleName), 0);
+	}
+
+	private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+	private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+		if (nCode >= 0) {
+			int vkCode = Marshal.ReadInt32(lParam);
+			if ((Keys)vkCode == Keys.LControlKey || (Keys)vkCode == Keys.RControlKey) {
+				if (wParam == WM_KEYDOWN) {
+					isControlDown = true;
+					//Cursor.Position = new Point(Cursor.Position.X + 10, Cursor.Position.Y);
+				} else if (wParam == WM_KEYUP) {
+					isControlDown = false;
+					//Cursor.Position = new Point(Cursor.Position.X - 10, Cursor.Position.Y);
+				}
+			} else if (wParam == WM_KEYDOWN) {
+				double speed = 10;
+				if (isControlDown) {
+					speed *= 3;
+				}
+				int _speed = (int)speed;
+				switch ((Keys)vkCode) {
+					case Keys.W:
+						//up
+						Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y - _speed);
+						break;
+					case Keys.S:
+						//down
+						Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y + _speed);
+						break;
+					case Keys.A:
+						//left
+						Cursor.Position = new Point(Cursor.Position.X - _speed, Cursor.Position.Y);
+						break;
+					case Keys.D:
+						//right
+						Cursor.Position = new Point(Cursor.Position.X + _speed, Cursor.Position.Y);
+						break;
+				}
+			}
+		}
+		return CallNextHookEx(hookID, nCode, wParam, lParam);
+	}
+
+	private const int WH_KEYBOARD_LL = 13;
+	private const int WM_KEYDOWN = 0x0100;
+	private const int WM_KEYUP = 0x0101;
+
+	[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+	private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+	[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+	[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+	private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+	[DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+	private static extern IntPtr GetModuleHandle(string lpModuleName);
+}
---
+++ KeyBoardMouseHookWFDemo/MouseActionSimulator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyBoardMouseHookWFDemo;
+
+internal static class MouseActionSimulator {
+	[DllImport("user32.dll", SetLastError = true)]
+	private static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct MOUSEINPUT {
+		public int dx;
+		public int dy;
+		public uint mouseData;
+		public uint dwFlags;
+		public uint time;
+		public IntPtr dwExtraInfo;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct INPUT {
+		public uint type;
+		public MOUSEINPUT mi;
+	}
+
+	private const uint MOUSEEVENTF_ABSOLUTE = 0x8000;
+	private const uint MOUSEEVENTF_MOVE = 0x0001;
+	private const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+	private const uint MOUSEEVENTF_LEFTUP = 0x0004;
+	private const int INPUT_MOUSE = 0;
+
+	public static void Click(int x, int y) {
+		// 获取屏幕分辨率
+		int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+		int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+
+		// 将屏幕坐标转换为相对坐标（0-65535）
+		int absoluteX = (int)((double)x / screenWidth * 65535);
+		int absoluteY = (int)((double)y / screenHeight * 65535);
+
+		INPUT[] inputs = new INPUT[3];
+
+		inputs[0].type = INPUT_MOUSE;
+		inputs[0].mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+		inputs[0].mi.dx = absoluteX;
+		inputs[0].mi.dy = absoluteY;
+
+		inputs[1].type = INPUT_MOUSE;
+		inputs[1].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+
+		inputs[2].type = INPUT_MOUSE;
+		inputs[2].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+
+		SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+	}
+
+	public static void Move(int x, int y) {
+		// 获取屏幕分辨率
+		int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+		int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+
+		// 将屏幕坐标转换为相对坐标（0-65535）
+		int absoluteX = (int)((double)x / screenWidth * 65535);
+		int absoluteY = (int)((double)y / screenHeight * 65535);
+
+		INPUT[] inputs = new INPUT[1];
+
+		inputs[0].type = INPUT_MOUSE;
+		inputs[0].mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+		inputs[0].mi.dx = absoluteX;
+		inputs[0].mi.dy = absoluteY;
+
+		SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+	}
+}
